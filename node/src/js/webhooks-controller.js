462b48e496b53f4fda980f7177c0b99eb0c38c7d
zkinion <zkinion@gmail.com>
2023-04-26 22:16:36 -0700

    updated working webhook

--- node/src/js/webhooks-controller.js
+++ node/src/js/webhooks-controller.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const dbHelper = require('./db-helper')
 const db = require('./models/index.js');
+const { Sequelize, Op } = require('sequelize');
 
 const utils = require('./utils')
 const kafkaHelper = require('./kafka-helper')
@@ -15,6 +16,81 @@ exports.getRouter = () => {
         const events = req.body
 
         console.log('Received hook events:')
+
+        try {
+            let subscriptionType = events[0].subscriptionType;
+            console.warn('subscriptionType: ', subscriptionType);
+            if (subscriptionType == 'contact.propertyChange') {
+
+                let propertyName = events[0].propertyName;
+                let hubspotId = events[0].objectId;
+                let eventValue = events[0].propertyValue;
+
+                // get user...
+                let professional = await db.sequelize.models.Professional.findOne({
+                    where: {
+                        hubspotId: {
+                            [Op.eq]: hubspotId,
+                        },
+                        // hubspotId: {
+                        //     [Op.eq]: 31457351
+                        // }
+                    },
+                });
+
+                // console.warn('professional found: ', professional);
+
+                if (propertyName == 'email') {
+
+                    if (professional) {
+                        professional.email = eventValue;
+                        await professional.save();
+                    }
+                }
+
+                if (propertyName == 'firstname') {
+
+                    if (professional) {
+                        professional.firstName = eventValue;
+                        await professional.save();
+                    }
+                }
+
+                if (propertyName == 'jobtitle') {
+
+                    if (professional) {
+                        professional.jobHeadline = eventValue;
+                        await professional.save();
+                    }
+                }
+
+                if (propertyName == 'mobilephone') {
+
+                    if (professional) {
+                        professional.phone = eventValue;
+                        await professional.save();
+                    }
+                }
+
+                if (propertyName == 'phone') {
+
+                    if (professional) {
+                        professional.phone = eventValue;
+                        await professional.save();
+                    }
+                }
+
+            }
+
+
+
+            } catch (error) {
+                console.error(error);
+            }
+
+
+
+
         utils.logJson(events)
         // await kafkaHelper.send(events)
         res.sendStatus(200)
